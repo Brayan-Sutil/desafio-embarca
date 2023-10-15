cf2f7ee3c28aa2888da2f70a5d96e44d582bfa27
BrayanSutil <brayansutil81@gmail.com>
2023-10-15 01:22:04 -0300

    feat: List characters

---
+++ src/components/Characters/Characters.tsx
@@ -0,0 +1,11 @@
+import { Paper, Typography } from "@mui/material";
+
+const Characters = () => {
+  return (
+    <Paper elevation={3} style={{ padding: "10px" }}>
+      <Typography>Luke Skywalker</Typography>
+    </Paper>
+  );
+};
+
+export default Characters;
